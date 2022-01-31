059b7439385c109a79e2e74022765cd0ec84a41e
w0dm4n <w0dm4n@hotmail.fr>
2022-01-31 08:03:39 +0100

    base metadata api

--- srcs/interfaces/env.ts
+++ srcs/interfaces/env.ts
@@ -2,8 +2,7 @@ interface EnvInterface {
     HTTP_HOST: string,
     HTTP_PORT: number,
     HOSTED_API_URL: string,
-    CACHE_LIVETIME_SECONDS: number,
-    CACHE_REFRESH_REQUESTS: number
+    MONGODB_URI: string,
 }
 
 export default (process.env as any) as EnvInterface
